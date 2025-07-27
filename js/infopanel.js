7a2ad93eeaa0103fe3ceeb1a7c1607ba899e8691
20korvin01 <korvin.venzke@gmail.com>
2025-07-27 15:26:29 +0200

    Add image galleries for all Canadian provinces
    Added image files for each Canadian province and territory and updated js/infopanel.js to include image gallery data for Alberta, Manitoba, Newfoundland and Labrador, New Brunswick, Nova Scotia, Ontario, Prince Edward Island, Quebec, Saskatchewan, British Columbia, Nunavut, and Northwest Territories. This enables province-specific image galleries in the info panel.

--- js/infopanel.js
+++ js/infopanel.js
@@ -11,6 +11,138 @@ closeInfoBtn.addEventListener('click', function() {
 
 // Bildergalerie Daten für Provinzen
 const provinceImages = {
+  "Alberta": [
+    './img/alberta/alberta1.jpeg',
+    './img/alberta/alberta2.jpeg',
+    './img/alberta/alberta3.jpg',
+    './img/alberta/alberta4.jpg',
+    './img/alberta/alberta5.jpg',
+    './img/alberta/alberta6.jpeg',
+    './img/alberta/alberta7.jpeg',
+    './img/alberta/alberta8.jpg',
+    './img/alberta/alberta9.jpg',
+    './img/alberta/alberta10.jpeg'
+  ],
+  "Manitoba": [
+    './img/manitoba/manitoba1.jpeg',
+    './img/manitoba/manitoba2.jpg',
+    './img/manitoba/manitoba3.jpg',
+    './img/manitoba/manitoba4.jpg',
+    './img/manitoba/manitoba5.jpeg',
+    './img/manitoba/manitoba6.jpeg',
+    './img/manitoba/manitoba7.jpg',
+    './img/manitoba/manitoba8.jpg',
+    './img/manitoba/manitoba9.jpeg',
+    './img/manitoba/manitoba10.jpeg'
+  ],
+  "Newfoundland and Labrador": [
+    './img/newfoundland_labrador/newfoundland_labrador1.jpeg',
+    './img/newfoundland_labrador/newfoundland_labrador2.jpg',
+    './img/newfoundland_labrador/newfoundland_labrador3.jpg',
+    './img/newfoundland_labrador/newfoundland_labrador4.jpg',
+    './img/newfoundland_labrador/newfoundland_labrador5.jpeg',
+    './img/newfoundland_labrador/newfoundland_labrador6.jpeg',
+    './img/newfoundland_labrador/newfoundland_labrador7.jpg',
+    './img/newfoundland_labrador/newfoundland_labrador8.jpg',
+    './img/newfoundland_labrador/newfoundland_labrador9.jpeg',
+    './img/newfoundland_labrador/newfoundland_labrador10.jpeg'
+  ],
+  "New Brunswick": [
+    './img/new_brunswick/new_brunswick1.jpeg',
+    './img/new_brunswick/new_brunswick2.jpg',
+    './img/new_brunswick/new_brunswick3.jpg',
+    './img/new_brunswick/new_brunswick4.jpg',
+    './img/new_brunswick/new_brunswick5.jpeg',
+    './img/new_brunswick/new_brunswick6.jpg',
+    './img/new_brunswick/new_brunswick7.jpg',
+    './img/new_brunswick/new_brunswick8.jpg',
+    './img/new_brunswick/new_brunswick9.jpg',
+    './img/new_brunswick/new_brunswick10.jpg'
+  ],
+  "Nova Scotia": [
+    './img/nova_scotia/nova_scotia1.jpeg',
+    './img/nova_scotia/nova_scotia2.jpg',
+    './img/nova_scotia/nova_scotia3.jpg',
+    './img/nova_scotia/nova_scotia4.jpg',
+    './img/nova_scotia/nova_scotia5.jpeg',
+    './img/nova_scotia/nova_scotia6.jpeg',
+    './img/nova_scotia/nova_scotia7.jpg',
+    './img/nova_scotia/nova_scotia8.jpg',
+    './img/nova_scotia/nova_scotia9.jpeg',
+    './img/nova_scotia/nova_scotia10.jpg'
+  ],
+  "Ontario": [
+    './img/ontario/ontario1.jpeg',
+    './img/ontario/ontario2.jpg',
+    './img/ontario/ontario3.jpg',
+    './img/ontario/ontario4.jpg',
+    './img/ontario/ontario5.jpeg',
+    './img/ontario/ontario6.jpeg',
+    './img/ontario/ontario7.jpg',
+    './img/ontario/ontario8.jpg',
+    './img/ontario/ontario9.jpeg',
+    './img/ontario/ontario10.jpeg'
+  ],
+  "Prince Edward Island": [
+    './img/prince_edward_island/prince_edward_island1.jpeg',
+    './img/prince_edward_island/prince_edward_island2.jpg',
+    './img/prince_edward_island/prince_edward_island3.jpg',
+    './img/prince_edward_island/prince_edward_island4.jpg',
+    './img/prince_edward_island/prince_edward_island5.jpeg',
+    './img/prince_edward_island/prince_edward_island6.jpeg',
+    './img/prince_edward_island/prince_edward_island7.jpg',
+    './img/prince_edward_island/prince_edward_island8.jpg',
+    './img/prince_edward_island/prince_edward_island9.jpeg',
+    './img/prince_edward_island/prince_edward_island10.jpeg'
+  ],
+  "Quebec": [
+    './img/quebec/quebec1.jpeg',
+    './img/quebec/quebec2.jpg',
+    './img/quebec/quebec3.jpg',
+    './img/quebec/quebec4.jpg',
+    './img/quebec/quebec5.jpeg',
+    './img/quebec/quebec6.jpeg',
+    './img/quebec/quebec7.jpg',
+    './img/quebec/quebec8.jpg',
+    './img/quebec/quebec9.jpg',
+    './img/quebec/quebec10.jpg'
+  ],
+  "Saskatchewan": [
+    './img/saskatchewan/saskatchewan1.jpeg',
+    './img/saskatchewan/saskatchewan2.jpg',
+    './img/saskatchewan/saskatchewan3.jpg',
+    './img/saskatchewan/saskatchewan4.jpg',
+    './img/saskatchewan/saskatchewan5.jpg',
+    './img/saskatchewan/saskatchewan6.jpg',
+    './img/saskatchewan/saskatchewan7.jpg',
+    './img/saskatchewan/saskatchewan8.jpg',
+    './img/saskatchewan/saskatchewan9.jpg',
+    './img/saskatchewan/saskatchewan10.jpg'
+  ],
+  "British Columbia": [
+    './img/british_columbia/bc1.jpeg',
+    './img/british_columbia/bc2.jpg',
+    './img/british_columbia/bc3.jpg',
+    './img/british_columbia/bc4.jpg',
+    './img/british_columbia/bc5.jpeg',
+    './img/british_columbia/bc6.jpeg',
+    './img/british_columbia/bc7.jpg',
+    './img/british_columbia/bc8.jpg',
+    './img/british_columbia/bc9.jpeg',
+    './img/british_columbia/bc10.jpeg'
+  ],
+  "Nunavut": [
+    './img/nunavut/nunavut1.jpeg',
+    './img/nunavut/nunavut2.jpg',
+    './img/nunavut/nunavut3.png',
+    './img/nunavut/nunavut4.jpg',
+    './img/nunavut/nunavut5.jpeg',
+    './img/nunavut/nunavut6.jpeg',
+    './img/nunavut/nunavut7.jpg',
+    './img/nunavut/nunavut8.png',
+    './img/nunavut/nunavut9.jpeg',
+    './img/nunavut/nunavut10.jpg'
+  ],
   'Yukon': [
     './img/yukon/yukon9.jpg',
     './img/yukon/yukon1.jpg',
@@ -22,8 +154,19 @@ const provinceImages = {
     './img/yukon/yukon7.jpg',
     './img/yukon/yukon10.jpg',
     './img/yukon/yukon8.jpg'
-  ]
-  // Weitere Provinzen können hier hinzugefügt werden
+  ],
+  'Northwest Territories': [
+    './img/northwest_territories/nwt1.jpeg',
+    './img/northwest_territories/nwt2.png',
+    './img/northwest_territories/nwt3.jpg',
+    './img/northwest_territories/nwt4.jpg',
+    './img/northwest_territories/nwt5.jpg',
+    './img/northwest_territories/nwt6.jpg',
+    './img/northwest_territories/nwt7.jpg',
+    './img/northwest_territories/nwt8.jpg',
+    './img/northwest_territories/nwt9.jpg',
+    './img/northwest_territories/nwt10.jpg'
+  ],
 };
 
 // Aktueller Galerie-Status
@@ -167,7 +310,6 @@ function showFeatureInfo(feature) {
       <p><strong>Typ:</strong> ${props.prov_type || 'Nicht verfügbar'}</p>
       <p><strong>Provinzcode:</strong> ${props.prov_code || 'Nicht verfügbar'}</p>
       <p><strong>Jahr:</strong> ${props.year || 'Nicht verfügbar'}</p>
-      <p><strong>Externer Link:</strong> <a href="https://www.breidenbach-education.com/" target="_blank" rel="noopener noreferrer">https://www.breidenbach-education.com/</a></p>
       <p>Weitere Informationen über diese Region werden hier angezeigt...</p>
     `;
     
